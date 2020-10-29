f0207155d9f4ec34dc5452878783c57a7705f9bc
unknown <tompsherman@gmail.com>
2020-10-29 19:19:44 -0400

    finding the typos

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express')
 const path = require('path')
 require('colors')
 
-const PORT = process.send.PORT || 8888
+const PORT = process.env.PORT || 8888
 
 const server = express()
 
